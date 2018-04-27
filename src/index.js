80a6b65458e05d954cc88c8f67f473887cadf0aa
nikolayev.sergii\User <nikolayev.sergii@gmail.com>
2018-04-27 18:58:26 +0300

    first component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './serega.js';
+import CategoryEditor from './serega.js';
 import './sasha.js';
 import './index.css';
 
 class App extends React.Component {
+    render(){
+        return(
+            <div>Hello world!
+                <CategoryEditor />
+            </div>
 
+        );
+    }
 }
 
 // ========================================
--- src/serega.js
+++ src/serega.js
@@ -1,1 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 console.log('serega js');
+
+export default class CategoryEditor extends React.Component {
+
+    render(){
+        return(
+            <div>
+                <input type="text" placeholder="color"/>
+                <input type="text" placeholder="Category name"/>
+                <select>
+                    <option value="Income">Income</option>
+                    <option value="Expense">Expense</option>
+                </select>
+                <button>Approve</button>
+            </div>
+        );
+    }
+}
